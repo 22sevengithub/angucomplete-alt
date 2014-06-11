47d054729a05f9caa86da8e896d9e0e63fd27c4d
Shaun O'Connell <shaun@22seven.com>
2014-06-11 12:19:49 +0200

    Added an optional 'input-type' attribute to support HTML5 input types such as 'search'.

--- angucomplete-alt.js
+++ angucomplete-alt.js
@@ -31,6 +31,7 @@ angular.module('angucomplete-alt', [] ).directive('angucompleteAlt', ['$parse', 
       descriptionField: '@',
       imageField: '@',
       inputClass: '@',
+      inputType: '@',
       pause: '@',
       searchFields: '@',
       minlength: '@',
@@ -39,7 +40,7 @@ angular.module('angucomplete-alt', [] ).directive('angucompleteAlt', ['$parse', 
       customOnBlur: '&customOnBlur',
       customOnFocus: '&customOnFocus'
     },
-    template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" ng-blur="onBlur($event)" ng-focus="onFocus($event)" type="text" placeholder="{{placeholder}}" class="{{inputClass}}"/><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results" ng-click="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
+    template: '<div class="angucomplete-holder"><input id="{{id}}_value" ng-model="searchStr" ng-blur="onBlur($event)" type="text" ng-focus="onFocus($event)" placeholder="{{placeholder}}" class="{{inputClass}}"/><div id="{{id}}_dropdown" class="angucomplete-dropdown" ng-if="showDropdown"><div class="angucomplete-searching" ng-show="searching">Searching...</div><div class="angucomplete-searching" ng-show="!searching && (!results || results.length == 0)">No results found</div><div class="angucomplete-row" ng-repeat="result in results" ng-click="selectResult(result)" ng-mouseover="hoverRow()" ng-class="{\'angucomplete-selected-row\': $index == currentIndex}"><div ng-if="imageField" class="angucomplete-image-holder"><img ng-if="result.image && result.image != \'\'" ng-src="{{result.image}}" class="angucomplete-image"/><div ng-if="!result.image && result.image != \'\'" class="angucomplete-image-default"></div></div><div class="angucomplete-title" ng-if="matchClass" ng-bind-html="result.title"></div><div class="angucomplete-title" ng-if="!matchClass">{{ result.title }}</div><div ng-if="result.description && result.description != \'\'" class="angucomplete-description">{{result.description}}</div></div></div></div>',
     link: function(scope, elem, attrs) {
       var inputField, minlength = MIN_LENGTH, searchTimer = null, lastSearchTerm = null;
 
@@ -277,6 +278,10 @@ angular.module('angucomplete-alt', [] ).directive('angucompleteAlt', ['$parse', 
           scope.$apply();
         }
       });
+
+      if (typeof scope.inputType != undefined) {
+        inputField.attr('type', scope.inputType);
+      }
     }
   };
 }]);
